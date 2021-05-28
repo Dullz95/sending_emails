ac0e8e5ae6be4e8590e26fbe05beab95d3a16e0f
Dullz95 <82204773+Dullz95@users.noreply.github.com>
2021-05-28 12:23:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 import smtplib
-
-s=smtplib.SMTP('smtp.gmail.com',587)
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+s = smtplib.SMTP('smtp.gmail.com',587)
 sender_email_id = 'abdullah.isaacs@gmail.com'
-reciever_email_id = 'calvertjustin1996@gmail.com'
+reciever_email_id = 'calvertjustin1996@gmail.com, beardedbigfoot19@gmail.com, jessenterblanche@gmail.com'
 password = input("enter pass: ")
+
+subject = "Hello All"
+msg = MIMEMultipart()
+msg ['from'] = sender_email_id
+msg ['To'] = reciever_email_id
+msg ['Subject'] = subject
+body = "Hi guys how are you"
+body = body + "How was your task yesterday"
+
+msg.attach(MIMEText(body, 'plain'))
+text = msg.as_string()
 s.starttls()
 s.login(sender_email_id,password)
-message = "Shut up"
-message = message + "please shut up"
-s.sendmail(sender_email_id,reciever_email_id, message)
+
+s.sendmail(sender_email_id,reciever_email_id, text)
 s.quit()
